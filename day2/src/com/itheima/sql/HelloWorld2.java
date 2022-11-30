1eaf7f17bb685e2a9932acf5ad277febc12bcbd9
hjh <13927711235@139.com>
2022-11-30 12:26:36 +0800

    first commit

---
+++ day2/src/com/itheima/sql/HelloWorld2.java
@@ -0,0 +1,9 @@
+package com.itheima.sql;
+
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+
+    }
+}//mysql 密码
---
+++ day2/src/com/itheima/sql/yunsuanfu.java
@@ -0,0 +1,15 @@
+package com.itheima.sql;
+
+import java.util.Scanner;
+
+public class yunsuanfu {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println(123 / 10);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数字");
+        int number = sc.nextInt();
+        System.out.println(number);
+    }
+}
+
